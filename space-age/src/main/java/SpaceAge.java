d53862aadd702c4a7c0723a8fa74ece0cbf528f4
00kaito <bednarz@programmer.net>
2017-11-20 21:58:30 +0100

    space age solution

---
+++ space-age/src/main/java/SpaceAge.java
@@ -0,0 +1,48 @@
+import static java.lang.StrictMath.round;
+
+class SpaceAge {
+        double sec;
+        long earthTimeDevider = 31557600;
+        private static final double MAXIMUM_DELTA = 1E-02;
+
+    SpaceAge(double seconds) {
+        sec = seconds;
+    }
+
+    double getSeconds() {
+        return sec;
+    }
+
+    double onEarth() {
+        return (getSeconds()/earthTimeDevider);
+    }
+
+    double onMercury() {
+            return onEarth()/0.2408467;
+    }
+
+    double onVenus() {
+        return onEarth()/0.61519726;
+    }
+
+    double onMars() {
+        return (onEarth()/1.8808158);
+    }
+
+    double onJupiter() {
+        return (onEarth()/11.862615);
+    }
+
+    double onSaturn() {
+        return (onEarth()/29.447498);
+    }
+
+    double onUranus() {
+        return (onEarth()/84.016846);
+    }
+
+    double onNeptune() {
+        return (onEarth()/164.79132);
+    }
+
+}
